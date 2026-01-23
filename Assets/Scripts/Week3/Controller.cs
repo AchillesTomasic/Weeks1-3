a20eff112049e8df7cea5baea673f323d17beb2e
SHERNET\tomasiac <tomasiac@sheridancollege.ca>
2026-01-23 17:26:28 -0500

    changes ending class week 3

--- Assets/Scripts/Week3/Controller.cs
+++ Assets/Scripts/Week3/Controller.cs
@@ -1,47 +1,78 @@
+using NUnit.Framework;
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.InputSystem;
 public class Controller : MonoBehaviour
 {
     public float posSpeed;
     public float rotSpeed;
+    public SpriteRenderer spriteRenderer;
+    public Color color;
+    public List<SpriteRenderer> controllableRend;
+    public Camera cam;
+    public List<Transform> controlledTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer.color = color;
+        bool isInsideSprite = spriteRenderer.bounds.Contains(transform.position);
+        controlledTransform.Add(transform);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 curMousePos = Mouse.current.position.ReadValue();
+        Vector3 worldMousePos = cam.ScreenToWorldPoint(curMousePos);
+        worldMousePos.z = 0;
+        bool isLeftMousePressed = Mouse.current.leftButton.IsPressed();
+        if(isLeftMousePressed)
+        {
+            // find any renderes currently hovered over
+            for (int i = 0; i < controllableRend.Count; i++)
+            {
+                bool ishovered = controllableRend[i].bounds.Contains(worldMousePos);
+                if (ishovered)
+                {
+                    controlledTransform.Add(controllableRend[i].transform);
+                }
+            }
+        }
+        for (int i = 0; i < controlledTransform.Count; i++)
+        {
+            Transform currentTransform = controlledTransform[i];
+            ControllingAssignment(currentTransform);
+        }
+
+
         
-        ControllingAssignment();
     }
-    void ControllingAssignment()
+    void ControllingAssignment(Transform transf)
     {
 
         bool upHeld = Keyboard.current.upArrowKey.isPressed;
         if (upHeld)
         {
 
-            transform.position += transform.up * posSpeed * Time.deltaTime;
+            transf.position += transform.up * posSpeed * Time.deltaTime;
 
         }
         bool downHeld = Keyboard.current.downArrowKey.isPressed;
         if (downHeld)
         {
 
-            transform.position -= transform.up * posSpeed * Time.deltaTime;
+            transf.position -= transform.up * posSpeed * Time.deltaTime;
         }
         bool leftRotHeld = Mouse.current.leftButton.isPressed;
         if (leftRotHeld)
         {
-            transform.eulerAngles += transform.forward * rotSpeed * Time.deltaTime;
+            transf.eulerAngles += transform.forward * rotSpeed * Time.deltaTime;
         }
         bool rightRotHeld = Mouse.current.rightButton.isPressed;
         if (rightRotHeld)
         {
-            transform.eulerAngles -= transform.forward * rotSpeed * Time.deltaTime;
+            transf.eulerAngles -= transform.forward * rotSpeed * Time.deltaTime;
         }
     }
 
